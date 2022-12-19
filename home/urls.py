c4069edb3111e9d408537d564deff02e5994d834
Saviour Bassey <saviourbazi2021@yahoo.com>
2022-12-19 16:29:50 +0100

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "home"
+
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home_view"),
+    path("about/", views.AboutView.as_view(), name="about_view"),
+    path("contact/", views.ContactView.as_view(), name="contact_view")
+]
